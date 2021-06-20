8ede49859501abf9a491dce932dcd84e0347e72d
fuzhenfang <alexfu@xiaoe-tech.com>
2021-06-20 21:34:35 +0800

    1111

---
+++ flaskTrstWeb/src/components/Postman/Postman.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+import { Card,Col,Row,Input,Select,Button,Tabs, Radio, Menu,Dropdown } from "antd";
+import { DownOutlined, SendOutlined } from "@ant-design/icons";
+
+const Option = Select.Option;
+const TabPane = Tabs.TabPane;
+
+const selectBefore = (
+    <Select defaultValue='GET' style={{width: 120, fontSize: 16, textAlign: 'left'}}>
+      <Option value='GET'>GET</Option>
+      <Option value='POST'>POST</Option>
+      <Option value='PUT'>PUT</Option>
+      <Option value='DELETE'>DELETE</Option>
+    </Select>
+  );
+
+
+
+export default() => {
+    const [bodyType, setBodyType] = useState("none");
+    const [rawType, setRawType] = useState("JSON");
+
+    const onClickMenu = key => {
+        setRawType(key);
+    }
+
+    const menu = (
+        <Menu>
+            <Menu.Item key="Text">
+                <a onClick={()=>{onClickMenu("Text")}}>Text</a>
+            </Menu.Item>
+            <Menu.Item key="JavaScript">
+                <a onClick={()=>{onClickMenu("JavaScript")}}>JavaScript</a>
+            </Menu.Item>
+            <Menu.Item key="JSON">
+                <a onClick={()=>{onClickMenu("JSON")}}>JSON</a>
+            </Menu.Item>
+            <Menu.Item key="HTML">
+                <a onClick={()=>{onClickMenu("HTML")}}>HTML</a>
+            </Menu.Item>
+            <Menu.Item key="XML">
+                <a onClick={()=>{onClickMenu("XML")}}>XML</a>
+            </Menu.Item>
+        </Menu>       
+    );
+    return (
+        <Card>
+            <Row gutter={[8,8]}> 
+                <Col span={18}>
+                    <Input size="large" addonBefore={selectBefore} defaultValue="mysite" />
+                </Col>
+                <Col span={6}>
+                    <Button type="primary" size="large" style={{ marginRight:16,float:'right' }}><SendOutlined />Send</Button>
+                </Col>
+            </Row>
+            <Row style={{marginTop: 8}}>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="Params" key="1">
+                        这是Params
+                    </TabPane>
+                    <TabPane tab="Headers" key="2">
+                        这是Headers
+                    </TabPane>
+                    <TabPane tab="Body" key="3">
+                        <Row>
+                            <Radio.Group defaultValue="none" value={bodyType} onChange={e => setBodyType(e.target.value)}>
+                                <Radio value="none">none</Radio>
+                                <Radio value="form-data">form</Radio>
+                                <Radio value="x-www-form-urlencoded">x-www-form-urlencoded</Radio>
+                                <Radio value="raw">raw</Radio>
+                                <Radio value="binary">binary</Radio>
+                                <Radio value="GraphQL">GraphQL</Radio>
+                            </Radio.Group>
+                            {
+                                bodyType == 'raw' ? <Dropdown style={{marginLeft: 8}} overlay={menu} trigger={['click']}>
+                                    <a onClick={e => e.preventDefault()}>
+                                        {rawType} <DownOutlined />
+                                    </a>
+                                </Dropdown>: null
+                            }
+                        </Row>
+                    </TabPane>
+                </Tabs>
+            </Row>
+        </Card>
+    )
+}
